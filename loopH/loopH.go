2642699ac7375ae12d8617d7390d4b944d9a3aa9
VictorWFS <victorwfs900@gmail.com>
2025-10-02 10:25:18 -0300

    feat:Estudos de loop em GO

---
+++ loopH/loopH.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	for horas := 0; horas <= 12; horas++ {
+		fmt.Println("Hora:  ", horas)
+
+		for min := 0; min < 60; min++ {
+			fmt.Println(min)
+		}
+	}
+}
