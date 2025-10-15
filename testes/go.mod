77bd85e6df9365d27c7ac0712156094059806d2f
VictorWFS <victorwfs900@gmail.com>
2025-10-15 11:39:58 -0300

    feat:Estudo sobre testes em GO

---
+++ testes/go.mod
@@ -0,0 +1,3 @@
+module github.com/seu-usuario/nome-do-projeto
+
+go 1.25.1
---
+++ testes/soma.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := soma(1, 2, 3)
+	y := Multiplica(10, 10)
+	fmt.Println(x, y)
+}
+
+func soma(i ...int) int {
+	total := 0
+	for _, v := range i {
+		total += v
+	}
+	return total
+}
+
+func Multiplica(i ...int) int {
+	total := 1
+	for _, v := range i {
+		total = total * v
+	}
+	return total
+}
